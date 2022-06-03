50eabbbb11e5e61fc2ab605dc56729e9003c7f52
Bex <ruthrebekahcupido@gmail.com>
2022-06-03 12:55:23 -0700

    File Updates

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,16 @@ int main(void)
 	char upper = 'A';
 
 	while (lower <= 'z')
-{
-	putchar(lower);
-	lower++;
-}
-while (upper <= 'Z')
-{
-	putchar(upper);
-	upper++;
-}
-putchar('\n');
-return (0);
+	{
+		putchar(lower);
+		lower++;
+	}
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+
+	return (0);
 }
