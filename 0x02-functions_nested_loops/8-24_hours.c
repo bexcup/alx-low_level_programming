1b765f2f1227af0f960474fea1b15559287e01c3
Bex <ruthrebekahcupido@gmail.com>
2022-06-07 08:18:01 -0700

    A day in Jack Bauer's life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of Jack Bauer's day
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hours ++;
+	}
+}
