9d326e763b0ca023fc6f3b97d734ab5f62b795eb
Bex <ruthrebekahcupido@gmail.com>
2022-06-07 04:47:24 -0700

    betty documentation style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
 *print alphabet - prints the alphabet, in lowercase, followed by a new line.
-*Description: Prints the alphabet with _putchar
+*Description: print_alphabet with _putchar
 *Return: void
 */
 void print_alphabet(void)
