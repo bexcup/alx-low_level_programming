5c275c2b0f7094b8c4882a4af25ddf2f42ebdbd8
Bex <ruthrebekahcupido@gmail.com>
2022-06-20 08:00:13 -0700

    Length of prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * @s: string that is checked
  * @accept: prefix that is measured
  *
- * Return: the number of bytes in the initial segment of s 
+ * Return: the number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
