dcd772d0c93a710cc2df6767fe32ea6e9f61ae46
Bex <ruthrebekahcupido@gmail.com>
2022-06-24 05:39:12 -0700

    Create Static Library

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: string that is checked
+ * @accept: prefix that is measured
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+				break;
+		}
+
+		if (!accept[y])
+			break;
+	}
+
+	return (x);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+Task 0. Create a static library
+Task 1. Write the script to create a static library
