c301d715431591b0e1e6c35425247589aad2e5a7
Bex <ruthrebekahcupido@gmail.com>
2022-06-30 11:21:38 -0700

    Loadshedding task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,58 +1,51 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
-int _strlen(char *string);
-
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-		;
-	return (i);
-}
-
 /**
- * *string_nconcat -  function that concatenates two strings
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
+ * string_nconcat - concatenates two strings.
+ * @s1: first string to copy
+ * @s2: second string to copy
+ * @n: number of bytes of s2 to copy
  *
- * Return: char pointer to newly allocated place in memory
+ * Return:If function fails - NULL,
+ * Otherwise: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int num, len, i, j;
+	unsigned int i, j, k;
+	char *s;
 
-	num = n;
+	if (s1 == NULL)
+		i = 0;
 
-	if (s1 == NULL) /* account for NULL strings */
-		s1 = "";
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
 
 	if (s2 == NULL)
-		s2 = "";
+		j = 0;
 
-	if (num < 0) /* account for negative n bytes */
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+
+	if (j > n)
+		j = n;
+
+	s = malloc(sizeof(char) * (i + j + 1));
+
+	if (s == NULL)
 		return (NULL);
 
-	if (num >= _strlen(s2)) /* account for n too big */
-		num = _strlen(s2);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
 
-	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
 
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++) /* concat */
-		ptr[i] = s1[i];
-
-	for (j = 0; j < num; j++)
-		ptr[i + j] = s2[j];
-	ptr[i + j] = '\0';
-
-	return (ptr);
+	return (s);
 }
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memmory for an array, given number of elements and size
+ * @nmemb - no. of elements
+ * @size - size of elements
+ *
+ * Return - If error - NULL,
+ * Otherwise return char pointer to newly allocated place in memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)ptr + i) = 0;
+
+	return (ptr);
+}
