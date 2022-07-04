ebb6a49a1e255d6e2dbe4c3cc93665b803f264fc
Bex <ruthrebekahcupido@gmail.com>
2022-07-04 08:19:02 -0700

    Requirement error

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,10 @@
 #ifndef SUM_H
 #define SUM_H
 
-/**
+/*
  * File: 4-sum.h
- * Desc: A header file that defines a function-like macro
- * SUM(x, y) that computes the sum of the numbers x and y
+ * Desc: A header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
