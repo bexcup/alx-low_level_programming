5afa911b7fcec9324f79ff90ad806178bbf597fc
Bex <ruthrebekahcupido@gmail.com>
2022-07-04 14:45:56 -0700

    Checker error fix

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: a variable of type struct dog
+ * @name: Name of the dog.
+ * @age: How old the dog is.
+ * @owner: Title of the person that owns the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,6 @@
 #define DOG_H
 
 /*
- * File: dog.h
- * Desc: A header file that defines a new type struct dog.
- */
-
-/*
  * struct dog - A new type that describes a dog.
  * @name: Name of the dog.
  * @age: How old the dog is.
