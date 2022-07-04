4596904f4ed930a93e8187fca406b9c2e9d883a1
Bex <ruthrebekahcupido@gmail.com>
2022-07-04 08:00:36 -0700

    Task 3 errors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,10 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 /**
  * File: 3-function_like_macro.h
- * Desc: A header file that defines a function-like macro ABS(x)
- * that computes the absolute value of a number x.
+ * Desc: A header file that defines a function-like 
+ * macro ABS(x) that computes the absolute value of a number x.
  */
 
 #define ABS(x) ((x) < 0 ? (-x) : (x))
