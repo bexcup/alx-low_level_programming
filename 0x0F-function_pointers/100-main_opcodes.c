fca74e2783c87904697dc03046ed82ee59aea367
Bex <ruthrebekahcupido@gmail.com>
2022-07-06 15:49:40 -0700

    Error fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,17 +9,27 @@
  */
 int main(int argc, char *argv[])
 {
+	int number_of_bytes, i;
+	char *func;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
-	if (atoi(argv[1]) < 0)
+	number_of_bytes = atoi(argv[1]);
+	if (number_of_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
+	func = (char *)&main;
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%.2hhx", func[i]);
+		if (i < number_of_bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 	return (0);
 }
