a48f1d8a534a92326a34dc5356db440bd1e7d7e9
Bex <ruthrebekahcupido@gmail.com>
2022-07-25 13:56:11 -0700

    Debugging code errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,18 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
 	{
 		while (text_content[y])
 			y++;
+		x = write(fd, text_content, y);
+		if (x != y)
+			return (-1);
 	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	x = write(fd, text_content, y);
-
-	if (fd == -1 || x == -1)
-		return (-1);
 
 	close(fd);
 
