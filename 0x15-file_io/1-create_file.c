ca39f25923328c73058de7fc5e7858f0ac9fd47a
Bex <ruthrebekahcupido@gmail.com>
2022-07-25 13:45:26 -0700

    Creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - Creates a file.
+ * @filename: Pointer to the file that is read.
+ * @text_content: Text to include in the file.
+ * If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, y = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+	{
+		while (text_content[y])
+			y++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(fd, text_content, y);
+
+	if (x != y)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
